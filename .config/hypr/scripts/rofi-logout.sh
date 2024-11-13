b778d026d51b3778bcf817de0252451e738b84d1
Dolbaeb777K <126266582+Dolbaeb777K@users.noreply.github.com>
2024-11-13 21:28:38 +0700

    Update rofi-logout.sh

--- .config/hypr/scripts/rofi-logout.sh
+++ .config/hypr/scripts/rofi-logout.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # logout dialog
 
-cmd=$(echo -e "suspend\nreboot\npoweroff" | rofi -width 350 -dmenu -p system:)
+cmd=$(echo -e "󰒲   Suspend\n󱄌   Reboot\n󰿅   Logout\n   Poweroff" | rofi -width 350 -dmenu -p system:)
 case $cmd in
     suspend)
         systemctl suspend ;;
     reboot)
         systemctl reboot ;;
+    logout)
+        hyprctl dispatch exit 0 ;;
     poweroff)
         systemctl poweroff ;;
 esac
